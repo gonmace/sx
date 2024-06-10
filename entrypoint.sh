86bf9e3f274e51716affa9b098300fa2ab8622f4
magoreal4 <magoreal4@gmail.com>
2024-06-10 09:40:55 -0400

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo 'Running collectstatic...'
-python manage.py collectstatic --noinput --settings=config.settings.prod
+python manage.py collectstatic --noinput --settings=settings.prod
 
 echo 'Running migrations...'
-python manage.py migrate --settings=config.settings.prod
+python manage.py migrate --settings=settings.prod
 
 echo 'Runing Server...'
 gunicorn settings.wsgi:application DJANGO_SETTINGS_MODULE=settings.prod --bind 0.0.0.0:8000
