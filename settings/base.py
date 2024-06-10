6a91b4bbe3ea84cb7b7f3c8a5235fbf005089363
magoreal4 <magoreal4@gmail.com>
2024-06-10 09:46:52 -0400

    first commit

--- settings/base.py
+++ settings/base.py
@@ -113,15 +113,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-SVG_DIRS=[
-    os.path.join(BASE_DIR, 'media/svg')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
